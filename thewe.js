e3453711fc654a683f52079f48d8df81e1bcb748
Avital Oliver <avital@thewe.net>
2009-07-27 04:15:03 +0000

    removed documentation from thewe.js

--- thewe.js
+++ thewe.js
@@ -1,30 +1,3 @@
-///////////////////////////////////////////////////////////////////////
-// theWE Wave Gadget Framework v0.1
-// Written by Avital Oliver (avital@thewe.net)
-//
-// Allows writing concurrent-safe gadgets and having a gadget state
-// is a full-fledged javascript object
-//
-// Must be loaded after MooTools!
-//
-// Initial documentation:
-// ----------------------
-// In order to use this framework, you must implement two functions:
-//
-// initialState():          returns a javascript object representing the initial state of the gadget
-//
-// stateUpdate(newState):   gets called when the state is updated
-//
-//
-// The functions thate theWE exposes are:
-//
-// thewe.alterState(alter): applies the alter function to the gadget state, for example
-//                          thewe.alterState(function(state) { state.count++ })
-//                          If the alter function returns an object, that object becomes the new state.
-//
-// thewe.getState():        Returns the "real" gadget state
-///////////////////////////////////////////////////////////////////////
-
 Array.implement({
 	max: function() {
 		var result = 0
